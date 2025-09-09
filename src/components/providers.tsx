8880bb26141d90ad0d650f25e501952d4435465e
Alain BERTRAND <alain.bertrand.mu@gmail.com>
2025-09-09 01:00:56 +0000

    Error: Hydration failed because the server rendered HTML didn't match th

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,9 +4,9 @@ import { Toaster } from "@/components/ui/toaster";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div>
       {children}
       <Toaster />
-    </>
+    </div>
   );
 }
